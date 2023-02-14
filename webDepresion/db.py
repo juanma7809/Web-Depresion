a905a55479e67b4c4ee1636acd40a62d55149a9c
Juan José Aroca Ariza <juanjose.aroca@utp.edu.co>
2023-02-14 10:37:07 -0500

    blog

---
+++ webDepresion/db.py
@@ -0,0 +1,10 @@
+MYSQL = DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_depresion',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
